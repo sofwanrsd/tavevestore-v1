76f96c489139ffbf317b0c298416b2d58034c4fe
Sofwan <sofwanrosidi02@gmail.com>
2026-07-01 11:37:34 +0700

    deploy

--- src/app/product/[id]/page.js
+++ src/app/product/[id]/page.js
@@ -2,15 +2,13 @@ import { notFound } from "next/navigation";
 import products from "@/data/products";
 import ProductClient from "./ProductClient";
 
-// SSG Dynamic Route Generator
 export async function generateStaticParams() {
   return products.map((product) => ({ id: product.id }));
 }
 
-// Fix: Destructure dengan explicit await
-export default async function ProductDetailPage(context) {
-  const { params } = context; // <--- ini fixnya
-  const product = products.find((p) => p.id === params.id);
+export default async function ProductDetailPage({ params }) {
+  const { id } = await params;
+  const product = products.find((p) => p.id === id);
 
   if (!product) return notFound();
 
